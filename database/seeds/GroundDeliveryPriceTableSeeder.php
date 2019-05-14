6335d3d88f6eedd781ac9140879e6b55b36fb206
ikorotkiy <korotkiy.iv@gmail.com>
2019-05-14 14:24:06 +0300

    add ground seeder

--- database/seeds/GroundDeliveryPriceTableSeeder.php
+++ database/seeds/GroundDeliveryPriceTableSeeder.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Models\Auction;
+use App\Models\GroundExitPort;
+use App\Models\GroundLocation;
 use Illuminate\Database\Seeder;
+use Illuminate\Filesystem\Filesystem;
 
 class GroundDeliveryPriceTableSeeder extends Seeder
 {
@@ -8,33 +12,39 @@ class GroundDeliveryPriceTableSeeder extends Seeder
      * Run the database seeds.
      *
      * @return void
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
      */
     public function run()
     {
-        $contents = \Illuminate\Support\Facades\Storage::get('ground.json');
-        $contents = collect(json_decode($contents));
+//        \DB::beginTransaction();
 
-        collect($contents)->each(function ($item, $key) {
-            if($key){
-                $auction = factory(\App\Models\Auction::class)->create(['name' => $key,]);
+        $uniqueLocation = [ ];
 
-                collect($item)->each(function ($item, $location) use($auction) {
-                    if($location){
-                        $groundLocation = \App\Models\GroundLocation::firstOrCreate(['name' => $location,]);
+        $contents = json_decode((new Filesystem())->get(storage_path('ground.json')));
 
-                        collect($item)->each(function ($price, $exitPort) use($auction,$groundLocation) {
-                            $exitPortEntity = \App\Models\GroundExitPort::firstOrCreate(['name' => $exitPort,]);
+        foreach ($contents as $auction => $auction_data) {
+            $auction = factory(Auction::class)->create(['name' => $auction]);
 
-                            factory(\App\Models\GroundDeliveryPrice::class)->create([
-                                'auction_id' => $auction->id,
-                                'ground_location_id' => $groundLocation->id,
-                                'ground_exit_port_id' => $exitPortEntity->id,
-                                'price' => (int) $price,
-                            ]);
-                        });
+            foreach ($auction_data as $exit_port => $exit_port_data) {
+                $exit_port = factory(GroundExitPort::class)->create(['name' => $exit_port]);
+
+                foreach ($exit_port_data as $location => $price) {
+                    if (isset($uniqueLocation[$location])) {
+                        $location = $uniqueLocation[$location];
+                    } else {
+                        $location = $uniqueLocation[$location] = factory(GroundLocation::class)->create(['name' => $location]);
                     }
-                });
+
+                    factory(App\Models\GroundDeliveryPrice::class)->create([
+                        'auction_id' => $auction->id,
+                        'ground_location_id' => $location->id,
+                        'ground_exit_port_id' => $exit_port->id,
+                        'price' => $price,
+                    ]);
+                }
             }
-        });
+        }
+
+//        \DB::rollBack();
     }
 }
